daa99a878696dd9931597bf34113822c6252f476
Takumi.Sakamoto <t.sakamoto.ouen@gmail.com>
2018-02-04 22:58:10 +0900

    [debug]ためしにpostbackのテキストを消してみる

--- handler/PostbackRequestHandler.php
+++ handler/PostbackRequestHandler.php
@@ -5,13 +5,8 @@ namespace ymdarake\tamai\bot\handler;
 use ymdarake\tamai\bot\handler\Handler;
 use ymdarake\tamai\bot\exception\UnhandleableEventTypeException;
 
-
-use ymdarake\lib\Logger;
-
 require_once(__DIR__ . "/Handler.php");
 require_once(dirname(__DIR__) . "/exception/UnhandleableEventTypeException.php");
-
-require_once(dirname(__DIR__) . "/lib/Logger.php");
 
 
 class PostbackRequestHandler extends Handler {
@@ -25,8 +20,6 @@ class PostbackRequestHandler extends Handler {
 		if ($this->event->type != "postback") {
 			throw new UnhandleableEventTypeException('postback', $this->event->type);
 		}
-
-		Logger::getInstance()->info($this->event->postback);
 
 		return $this->replyMultiTextMessages($this->event->postback->data);
 	}
